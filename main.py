18924e34b3e1233a7815edb286a72503df13a4a9
ayeo <me@ayeo.pl>
2019-12-02 00:18:41 +0100

    Dev

--- main.py
+++ main.py
@@ -2,20 +2,16 @@ import random
 
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, ReferenceListProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
 
 
-class Test(Widget):
-    pass
-
-class Buttons(Widget):
-    pass
-
 class Car(Widget):
     angle = NumericProperty(0)
-    velocity = (0, 0)
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
 
     def move(self, rotation):
         self.pos = Vector(self.velocity) + self.pos
@@ -23,18 +19,24 @@ class Car(Widget):
 
 class Game(Widget):
     car = None
-    test = None
-    buttons = None
     action2rotation = [0, 20, -20]
+    init = True
+
+    def set_car(self, car, pos):
+        self.car = car
+        self.car.pos = pos
+        self.add_widget(car)
 
     def update(self, dt):
+        if (self.init):
+            self.set_car(Car(), self.center)
+            self.init = False
+
         action = random.randint(0, 2)
         rotation = self.action2rotation[action]
         self.car.move(rotation)
         self.car.velocity = Vector(5, 0).rotate(self.car.angle)
 
-class MyPaintWidget(Widget):
-    pass
 
 class CarApp(App):
 
