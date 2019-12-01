49b9b86403972d10bcee63e589e626ea4a68f4a9
ayeo <me@ayeo.pl>
2019-12-01 21:11:31 +0100

    Initial

---
+++ main.py
@@ -0,0 +1,45 @@
+import random
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
+
+
+class Car(Widget):
+    angle = NumericProperty(0)
+    velocity = (0, 0)
+
+    def move(self, rotation):
+        self.pos = Vector(self.velocity) + self.pos
+        self.angle = self.angle + rotation
+        # self.sensor1 = Vector(30, 0).rotate(self.angle) + self.pos
+        # self.sensor2 = Vector(30, 0).rotate((self.angle + 30) % 360) + self.pos
+        # self.sensor3 = Vector(30, 0).rotate((self.angle - 30) % 360) + self.pos
+
+
+class Game(Widget):
+    car = None
+    action2rotation = [0, 20, -20]
+
+    def update(self, dt):
+        action = random.randint(0, 2)
+        rotation = self.action2rotation[action]
+        self.car.move(rotation)
+        self.car.velocity = Vector(6, 0).rotate(self.car.angle)
+
+class MyPaintWidget(Widget):
+    pass
+
+class CarApp(App):
+
+    def build(self):
+        parent = Game()
+        Clock.schedule_interval(parent.update, 1.0 / 60.0)
+        return parent
+
+
+# Running the whole thing
+if __name__ == '__main__':
+    CarApp().run()
