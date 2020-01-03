ea1bdf84fd7b6e96d4c9f7dc0d51bf97bce79824
ayeo <me@ayeo.pl>
2020-01-03 17:58:53 +0100

    Get rid of kivy

--- main.py
+++ main.py
@@ -1,52 +1,26 @@
-import random
+import pygame
 
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty
-from kivy.vector import Vector
-from kivy.clock import Clock
+FPS = 60
+SIZE = 300
+TAIL = 2
+ANTS = 20
 
-
-class Car(Widget):
-    angle = NumericProperty(0)
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
-    velocity = ReferenceListProperty(velocity_x, velocity_y)
-
-    def move(self, rotation):
-        self.pos = Vector(self.velocity) + self.pos
-        self.angle = self.angle + rotation
-
-class Game(Widget):
-    car = None
-    action2rotation = [0, 20, -20]
-    init = True
-
-    def set_car(self, car, pos):
-        self.car = car
-        self.car.pos = pos
-        self.add_widget(car)
-
-    def update(self, dt):
-        if (self.init):
-            self.set_car(Car(), self.center)
-            self.init = False
-
-        action = random.randint(0, 2)
-        rotation = self.action2rotation[action]
-        self.car.move(rotation)
-        self.car.velocity = Vector(5, 0).rotate(self.car.angle)
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((SIZE * TAIL, SIZE * TAIL))
+pygame.display.set_caption("Karaluch")
+clock = pygame.time.Clock()
 
 
-class CarApp(App):
+running = True
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-    def build(self):
-        parent = Game()
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+    pygame.time.delay(100)
 
-        Clock.schedule_interval(parent.update, 1.0 / 60.0)
-        return parent
-
-
-# Running the whole thing
-if __name__ == '__main__':
-    CarApp().run()
+pygame.quit()
